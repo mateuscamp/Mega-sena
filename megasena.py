f85614c038b428b8880930910a8980db0974c984
Mateus Campelo <mateuscampelob@gmail.com>
2026-03-29 17:08:44 -0300

    v2.0: Adicionado loop de repetição e tratamento de erro

--- megasena.py
+++ megasena.py
@@ -1,23 +1,45 @@
 from random import sample
-from rich import print
+from rich.console import Console
 from rich.panel import Panel
 from rich.rule import Rule
-from rich.align import Align
 from time import sleep
 
-geral = []
-print(Panel.fit('Gerador de jogos [green]MEGA SENNA[/green]', border_style = 'magenta'))
-print()
-quant = int(input('Quantos jogos gerar? '))
+console = Console()
 
-for c in range(0, quant):
-    jogo = sorted(sample(range(1, 61), 6))
-    geral.append(jogo)
-    print(f'[yellow]Jogo {c + 1}:[/yellow] [green]{geral[c]}[/green]')
+def executar_gerador():
+    geral = []
+    
+    console.print(Panel.fit('Gerador de jogos [green]MEGA SENA[/green]', border_style='magenta'))
+    console.print()
+
+    try:
+        quant = int(console.input('[bold white]Quantos jogos deseja gerar agora? [/bold white]'))
+    except ValueError:
+        console.print("[bold red]Opção inválida! Digite apenas números inteiros.[/bold red]")
+        return # Volta para o início do loop
+
+    # Loop de geração dos jogos
+    for c in range(0, quant):
+        jogo = sorted(sample(range(1, 61), 6))
+        geral.append(jogo)
+        console.print(f'[yellow]Jogo {c + 1}:[/yellow] [green]{geral[c]}[/green]')
+        sleep(0.5)
+
+    console.print()
+    console.print(Rule(style="bold cyan"))
+    console.print(Rule(title="[bold gold1]🍀 BOA SORTE NO SORTEIO! 💰[/bold gold1]", style="bold green"))
+    console.print(Rule(style="bold cyan"))
+
+#Loop principal
+while True:
+    executar_gerador()
+    
     print()
-    sleep(1)
-
-print()
-print(Rule(style="bold cyan"))
-print(Rule(title="[bold gold1]🍀 BOA SORTE NO SORTEIO! 💰[/bold gold1]", style="bold green"))
-print(Rule(style="bold cyan"))
+    decisao = console.input("[bold cyan]Deseja gerar mais jogos? (s/n): [/bold cyan]").strip().lower()
+    
+    if decisao != 's':
+        console.print("\n[bold magenta]Obrigado por usar o Gerador! Até a próxima.[/bold magenta]")
+        sleep(2)
+        break
+    
+    console.clear()
